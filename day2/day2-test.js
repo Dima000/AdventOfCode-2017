a437afffb3090357777415df3ad3f47bbbaaaabe
Dumitru Motpan <dumitru.motpan@digital1.online>
2017-12-05 13:21:54 +0200

    day2

--- day2/day2-test.js
+++ day2/day2-test.js
@@ -1,14 +1,15 @@
-let main = require('./day1.js');
+let main = require('./day2.js');
 
 let test1 =
     '5 1 9 5\n' +
     '7 5 3\n' +
     '2 4 6 8';
 
+let test2 =
+    '5 9 2 8\n' +
+    '9 4 7 3\n' +
+    '3 8 6 5';
+
 main.firstTask(test1);  //result 18
 
-
-let test3 = '123123';
-let test4 = '12131415';
-main.secondTask(test3);
-main.secondTask(test4);
+main.secondTask(test2); //result 9
--- day2/day2.js
+++ day2/day2.js
@@ -1,28 +1,48 @@
 function firstTask(data) {
-  let lines = data.split('\n');
-  let parsedLines = [];
-
-  for(let l of lines) {
-  }
-
   let sum = 0;
+  let lines = data.split('\n');
 
+  for (let l of lines) {
+    let lineNumbers = l.match(/\d+/g);
+
+    let min = Math.min(...lineNumbers);
+    let max = Math.max(...lineNumbers);
+
+    sum += max - min;
+  }
 
   console.log(`first task: ${sum}`);
 }
 
 function secondTask(data) {
   let sum = 0;
+  let lines = data.split('\n');
+
+  for (let l of lines) {
+    let found = false;
+    let lineNumbers = l.match(/\d+/g).sort((a, b) => b - a);
+
+
+    for (let i = 0; i < lineNumbers.length - 1; i++) {
+      if (found) break;
+      for (let j = i + 1; j < lineNumbers.length; j++) {
+        if (lineNumbers[i] % lineNumbers[j] === 0) {
+          sum += lineNumbers[i] / lineNumbers[j];
+          found = true;
+          break;
+        }
+      }
+    }
+  }
 
   console.log(`second task: ${sum}`);
 }
 
 
-
 let readFile = require('../util/readFile.js');
 
-readFile('day1-1.txt', firstTask);
-readFile('day1-2.txt', secondTask);
+readFile('day2-1.txt', firstTask);
+readFile('day2-2.txt', secondTask);
 
 module.exports.firstTask = firstTask;
 module.exports.secondTask = secondTask;
